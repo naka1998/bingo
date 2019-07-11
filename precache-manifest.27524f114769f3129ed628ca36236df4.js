bf31e5674a1212df4720ee4fcaff1ced055977c0
naka1998 <bjtn1290411@gn.iwasaki.ac.jp>
2019-07-11 09:12:46 +0900

    Updates

---
+++ precache-manifest.27524f114769f3129ed628ca36236df4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0b021710eb78d3b21d1fc9f7c38edd9",
+    "url": "/bingo/index.html"
+  },
+  {
+    "revision": "a393f3cb180d1cc233ff",
+    "url": "/bingo/static/css/main.efd34c47.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/bingo/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "a393f3cb180d1cc233ff",
+    "url": "/bingo/static/js/main.58e9c20a.chunk.js"
+  },
+  {
+    "revision": "5f9d2c01300bfd8d429a",
+    "url": "/bingo/static/js/runtime~main.d550205c.js"
+  }
+]);
